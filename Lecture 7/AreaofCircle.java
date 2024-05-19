c1f83f8444423d9d8bac2a320355a0e399d032ec
ravisingh-0 <ravi.singh@soekalkaji.co.in>
2024-05-20 01:25:01 +0530

    Lecture 7 in class

---
+++ Lecture 7/AreaofCircle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AreaofCircle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        System.out.println("The sum is " + (3.14f*radius*radius));
+    }
+}
---
+++ Lecture 7/Sumof2no.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sumof2no{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("The sum is " + (a+b));
+    }
+}
